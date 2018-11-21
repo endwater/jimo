87664179f68383e194a8092a447d22865cbffcd7
endwater <43136657+endwater@users.noreply.github.com>
2018-11-21 15:25:47 -0800

    Update item.sql

--- item.sql
+++ item.sql
@@ -39,3 +39,95 @@ VALUE (i_item_id);
 
 create map if not exists item_map_i_item_desc on item_table
 KEY (i_item_sk)
+VALUE (i_item_desc);
+
+create map if not exists item_map_i_current_price on item_table
+KEY (i_item_sk)
+VALUE (i_current_price);
+
+create map if not exists item_map_i_wholesale_cost on item_table
+KEY (i_item_sk)
+VALUE (i_wholesale_cost);
+
+create map if not exists item_map_i_brand_id on item_table
+KEY (i_item_sk)
+VALUE (i_brand_id);
+
+create map if not exists item_map_i_brand on item_table
+KEY (i_item_sk)
+VALUE (i_brand);
+
+create map if not exists item_map_i_class_id on item_table
+KEY (i_item_sk)
+VALUE (i_class_id);
+
+create map if not exists item_map_i_class on item_table
+KEY (i_item_sk)
+VALUE (i_class);
+
+create map if not exists item_map_i_category_id on item_table
+KEY (i_item_sk)
+VALUE (i_category_id);
+
+create map if not exists item_map_i_category on item_table
+KEY (i_item_sk)
+VALUE (i_category);
+
+create map if not exists item_map_i_manufact_id on item_table
+KEY (i_item_sk)
+VALUE (i_manufact_id);
+
+create map if not exists item_map_i_manufact on item_table
+KEY (i_item_sk)
+VALUE (i_manufact);
+
+create map if not exists item_map_i_size on item_table
+KEY (i_item_sk)
+VALUE (i_size);
+
+create map if not exists item_map_i_formulation on item_table
+KEY (i_item_sk)
+VALUE (i_formulation);
+
+create map if not exists item_map_i_color on item_table
+KEY (i_item_sk)
+VALUE (i_color);
+
+create map if not exists item_map_i_units on item_table
+KEY (i_item_sk)
+VALUE (i_units);
+
+create map if not exists item_map_i_container on item_table
+KEY (i_item_sk)
+VALUE (i_container);
+
+create map if not exists item_map_i_manager_id on item_table
+KEY (i_item_sk)
+VALUE (i_manager_id);
+
+create map if not exists item_map_i_product_name on item_table
+KEY (i_item_sk)
+VALUE (i_product_name);
+
+create job item_job(2)
+begin
+create dataset file item_dataset
+(
+    schema:item_schema,
+    files:(
+            (filename:"/tpcds/data1G/item.dat",serverid:0)
+            ),
+    splitter:(
+                block_size:10000
+            )
+);
+
+create dataproc load_data item_doc
+(
+    input:item_dataset,
+    table:item_table
+);
+
+end;
+
+run job item_job(threadnum:6,processnum:3);
